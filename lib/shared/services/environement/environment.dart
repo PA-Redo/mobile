a3597d1da79a5c24962981b4f144790300ce431b
nainssa <paulabib17@gmail.com>
2023-09-22 12:56:20 +0200

    start

--- lib/shared/services/environement/environment.dart
+++ lib/shared/services/environement/environment.dart
@@ -1,7 +1,7 @@
 class Environment {
   static String get apiURL => const String.fromEnvironment(
         'API_URL',
-        defaultValue: 'https://api.pa-crx.fr:443',
-        //defaultValue: 'http://localhost:8080',
+        //defaultValue: 'https://api.nainssa.fr:443',
+        defaultValue: 'http://localhost:8080',
       );
 }
