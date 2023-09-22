058bc4f024cacd7c8ea9d42c7a459335d4e6ddf6
nainssa <paulabib17@gmail.com>
2023-09-22 13:07:13 +0200

    remove back port on env file

--- lib/shared/services/environement/environment.dart
+++ lib/shared/services/environement/environment.dart
@@ -1,7 +1,7 @@
 class Environment {
   static String get apiURL => const String.fromEnvironment(
         'API_URL',
-        //defaultValue: 'https://back.nainssa.fr:443',
+        //defaultValue: 'https://back.nainssa.fr',
         defaultValue: 'http://localhost:8080',
       );
 }
