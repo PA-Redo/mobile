f3574d7be2869bf8f462133b610aa9852e40c0b5
nainssa <paulabib17@gmail.com>
2023-09-22 13:05:06 +0200

    .new back name

--- lib/shared/services/environement/environment.dart
+++ lib/shared/services/environement/environment.dart
@@ -1,7 +1,7 @@
 class Environment {
   static String get apiURL => const String.fromEnvironment(
         'API_URL',
-        //defaultValue: 'https://api.nainssa.fr:443',
+        //defaultValue: 'https://back.nainssa.fr:443',
         defaultValue: 'http://localhost:8080',
       );
 }
