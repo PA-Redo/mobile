63135a0bc540940408830f7798a398aa05e7aa1b
nainssa <paulabib17@gmail.com>
2023-11-20 02:11:20 +0100

    fix

--- lib/flows/chat/chat_screen.dart
+++ lib/flows/chat/chat_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pa_mobile/core/model/chat.dart';
 import 'package:pa_mobile/core/model/message.dart';
