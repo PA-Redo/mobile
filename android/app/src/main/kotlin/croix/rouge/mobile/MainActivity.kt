033236a6c0c9b2f59d3462df7fd186beb5b8ea52
nainssa <paulabib17@gmail.com>
2023-11-21 06:14:08 +0100

    fix

--- android/app/src/main/kotlin/croix/rouge/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/croix/rouge/mobile/MainActivity.kt
@@ -1,6 +1,7 @@
 package croix.rouge.mobile.com
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
--- test/app_test.dart
+++ test/app_test.dart
@@ -1,14 +1,20 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pa_mobile/app.dart';
 import 'package:pa_mobile/flows/home/ui/home_screen.dart';
 
 void main() {
+  final navigatorKey = GlobalKey<NavigatorState>();
   group('App', () {
     testWidgets('renders', (tester) async {
+      //create key
       await tester.pumpWidget(
-        const MyApp(
+        MyApp(
           isLogged: false,
           isVolunteer: false,
+          navigatorKey: navigatorKey,
         ),
       );
       expect(find.byType(HomeScreen), findsOneWidget);
