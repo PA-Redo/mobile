c73ca4b288812234cf8c2d586abf635c281aa05e
nainssa <paulabib17@gmail.com>
2023-11-21 02:36:59 +0100

    fix

--- android/app/src/main/kotlin/croix/rouge/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/croix/rouge/mobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.verygoodcore.mobile
+package croix.rouge.mobile.com
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:pa_mobile/app.dart';
+import 'package:pa_mobile/shared/firebase/firebase_api.dart';
 import 'package:pa_mobile/shared/services/storage/jwt_secure_storage.dart';
 import 'package:pa_mobile/shared/services/storage/stay_login_secure_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Stripe.publishableKey =
       'pk_test_51Nt6kLAMwqibCvaHmV7QHn8TywuAhZM0CG0kSTCm5BVM0JKRxRheV3HaqKgZC2j13cQNlGJnV4SneBSZtHxHc3NM00pHQS5Iur';
   await dotenv.load(fileName: 'assets/.env');
+  await Firebase.initializeApp();
+  await FirebaseApi().initNotification();
   final isLogged = await autoLogin();
   print(isLogged);
   final isVolunteer = await isVolunteers();
